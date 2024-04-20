225c77f4760df94544164b4adb46edc34879aa0f
alriffaud <100241090+alriffaud@users.noreply.github.com>
2024-04-20 14:42:51 -0300

    Modification of 0-gather_data_from_an_API.py file.

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,20 +9,22 @@ if __name__ == "__main__":
     import requests
     from sys import argv
 
-    NUMBER_OF_DONE_TASKS = 0
-    TASK_TITLE = []
-    r = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                     format(argv[1])).json()
-    EMPLOYEE_NAME = r.get("name")
-    r = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                     format(argv[1])).json()
-    for i in r:
-        if i.get("completed") is True:
-            TASK_TITLE.append(i.get("title"))
-            NUMBER_OF_DONE_TASKS += 1
-    TOTAL_NUMBER_OF_TASKS = len(r)
-
-    print("Employee {} is done with tasks({}/{}):".
-          format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-    for title in TASK_TITLE:
-        print("\t {}".format(title))
+    if len(argv) > 1:
+        user = argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        req = requests.get("{}users/{}".format(url, user))
+        name = req.json().get("name")
+        if name is not None:
+            jreq = requests.get(
+                "{}todos?userId={}".format(
+                    url, user)).json()
+            alltsk = len(jreq)
+            completedtsk = []
+            for t in jreq:
+                if t.get("completed") is True:
+                    completedtsk.append(t)
+            count = len(completedtsk)
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(name, count, alltsk))
+            for title in completedtsk:
+                print("\t {}".format(title.get("title")))
